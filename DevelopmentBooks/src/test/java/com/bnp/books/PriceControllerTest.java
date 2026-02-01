b5c99f144f723b8cb778e47da109c08d02c33b11
Muhammad imran <iimrankhn@gmail.com>
2026-02-01 20:46:12 +0100

    test: three different books get 10 percent discount [GREEN]

--- DevelopmentBooks/src/test/java/com/bnp/books/PriceControllerTest.java
+++ DevelopmentBooks/src/test/java/com/bnp/books/PriceControllerTest.java
@@ -47,6 +47,21 @@ public class PriceControllerTest {
 		assertPrice(requestBody, "95.0");
 	}
 	
+	@Test
+	@DisplayName("three different books get 10 percent discount")
+	void threeDifferentBooksGet10PercentDiscount() throws Exception {
+
+		String requestBody = """
+				[
+				  { "name": "CLEAN_CODE", "price": 50 },
+				  { "name": "CLEAN_CODER", "price": 50 },
+				  { "name": "CLEAN_ARCHITECTURE", "price": 50 }
+				]
+				""";
+
+		assertPrice(requestBody, "135.0");
+	}
+	
 	private void assertPrice(String requestBody, String price) throws Exception {
 		mockMvc.perform(post(API_V1_PRICE).contentType(MediaType.APPLICATION_JSON).content(requestBody))
 				.andExpect(status().isOk()).andExpect(content().string(price));

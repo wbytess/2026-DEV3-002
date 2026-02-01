bd2577119e1b72a55a026932348108f82188f6d4
Muhammad imran <iimrankhn@gmail.com>
2026-02-01 21:02:20 +0100

    test: assignment example basket should cost 320 [GREEN]

--- DevelopmentBooks/src/test/java/com/bnp/books/PriceControllerTest.java
+++ DevelopmentBooks/src/test/java/com/bnp/books/PriceControllerTest.java
@@ -95,6 +95,23 @@ public class PriceControllerTest {
 		assertPrice(requestBody, "187.5");
 	}
 	
+	@Test
+	@DisplayName("assignment example basket should cost 320")
+    void assignmentExample() throws Exception {
+
+		String requestBody = """
+				[
+				  { "name": "CLEAN_CODE", "price": 50 },{ "name": "CLEAN_CODE", "price": 50 },
+				  { "name": "CLEAN_CODER", "price": 50 },{ "name": "CLEAN_CODER", "price": 50 },
+				  { "name": "CLEAN_ARCHITECTURE", "price": 50 },{ "name": "CLEAN_ARCHITECTURE", "price": 50 },
+				  { "name": "TDD_BY_EXAMPLE", "price": 50 },
+				  { "name": "LEGACY_CODE", "price": 50 }
+				]
+				""";
+
+		assertPrice(requestBody, "320.0");
+	}
+	
 	private void assertPrice(String requestBody, String price) throws Exception {
 		mockMvc.perform(post(API_V1_PRICE).contentType(MediaType.APPLICATION_JSON).content(requestBody))
 				.andExpect(status().isOk()).andExpect(content().string(price));
